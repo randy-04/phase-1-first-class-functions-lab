7718a88d87bfd8c7ef317d3f6014fb59753693d2
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-23 13:29:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0,2);
+
+}
+
+const returnLastTwoDrivers = function (array) {
+    return array.slice(-2);
+} 
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(int) {
+    return function (fare) {
+        return int * fare;
+    }
+}
+
+
+
+function fareDoubler(fare) {
+    const fm = createFareMultiplier(2);
+    return fm(fare);
+} 
+
+function fareTripler(fare) {
+    const ft= createFareMultiplier(3);
+    return ft(fare);
+}
+
+function selectDifferentDrivers(array, f) {
+    return f(array);
+
+}
